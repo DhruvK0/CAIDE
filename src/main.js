d132ac3f385e52e8de9432fbfecd69cc8393685c
Dhruv Kanetkar <kanetkard@gmail.com>
2023-12-25 20:07:13 -0800

    push test for tap drill sizes

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow, clipboard, Menu } = require('electron')
+
 
 const createWindow = () => {
   const win = new BrowserWindow({
@@ -6,12 +7,40 @@ const createWindow = () => {
     height: 600
   })
 
-  win.loadFile('src/index.html')
+  win.loadFile('src/TapDrillSizes.html')
+
+  win.on('closed', function () {
+    win = null;
+  });
+
+  // Add event listener to capture the selected text and copy to clipboard
+  win.webContents.on('didFinishLoad', () => {
+    win.webContents.on('system-context-menu', (_, params) => {
+      const { selectionText } = params;
+
+      if (selectionText && selectionText.trim() !== '') {
+        const contextMenuTemplate = [
+          {
+            label: 'Copy',
+            click: () => {
+              clipboard.writeText(selectionText);
+            },
+          },
+        ];
+
+        const contextMenu = Menu.buildFromTemplate(contextMenuTemplate);
+        contextMenu.popup({ window: win });
+      }
+    });
+  });
 }
+
+// mainWindow.loadFile('TapDrillSizes.html')
 
 app.whenReady().then(() => {
   createWindow()
 })
+// 
 
 // app.on('window-all-closed', () => {
 //     if (process.platform !== 'darwin') app.quit()
